53938c1f09ba5423291b40bdce524c95c14f72a4
Veer Pratap <veerpratap@example.com>
2025-09-08 22:41:05 +0530

    while loop

---
+++ src/loops/whileloop.java
@@ -0,0 +1,70 @@
+package loops;
+import  java.util.Scanner;
+public class whileloop {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number:");
+        int n = sc.nextInt();
+//        int num=5;
+//        while ( num<=n){
+//            System.out.println(num);
+//            num++;
+//        }
+//        int sum=0;
+//        while (n>=num){
+//            sum = sum+n;
+//            num++;
+//        }
+//        System.out.println("Sum of natural numbers:"+sum);
+        //table
+//        while (num<=n){
+//            System.out.println("");
+//        }
+//        ********************   for loop **********************
+        // for loop
+//        for (int i=1; i<=n;i++){
+//            System.out.println(i);
+//        }
+//            int sum =0;
+//        for (int i=1; i<=n;i++){
+//            sum= sum+i;
+//        }
+//        System.out.println("sum:"+sum);
+
+        // table
+
+//        for (int i= 0;i<=10;i++){
+//            System.out.println(n+"*"+i+"="+n*i);
+//        }
+
+//        ****************    Do while   *******************
+            // check numbrr above than 10
+//        do {
+//            System.out.println(n);
+//            n++;
+//        }while (n<=10);
+//        int sum=0;
+//        int i=1;
+//        do {
+//           sum= sum+i;
+//            i++;
+//        }while (n>=i);
+//        System.out.println("sum--"+sum);
+
+        // table
+//        int i=0;
+//        do {
+//            System.out.println(n+"*"+i+"="+(n*i));
+//            i++;
+//        }while (i<=10);
+
+        ///  typecasting
+//        char ch ='a';
+//        int x=ch; //  implicit typecsting
+
+        char veer = 'a';
+        int y = (int)veer;  ///exlicit typecasting
+
+
+    }
+}
